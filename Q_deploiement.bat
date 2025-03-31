a75b3321b9fb47743fdbc569a73afbb24289f8e8
David de Lorenzo <de.lorenzo.david@gmail.com>
2025-03-31 14:12:21 +0200

    installeur setup.exe

---
+++ Q_deploiement.bat
@@ -0,0 +1,4 @@
+@echo Run me in Qt Creator terminal .. "run environnement"
+
+windeployqt --release  build\Desktop_Qt_6_6_1_MSVC2019_64bit-Release\_release
+
---
+++ Q_documentation.bat
@@ -0,0 +1,5 @@
+@echo Run me in Qt Creator terminal .. "run environnement"
+
+qdoc TiCalcul.qdocconf
+
+qhelpgenerator docs/qdoc/html/TiCalcul.qhp -o TiCalcul.qch
