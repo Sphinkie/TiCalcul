2c5d2be6f6415aebbd8a8c0a11e882b1697ebea1
David de Lorenzo <de.lorenzo.david@gmail.com>
2025-04-01 14:59:47 +0200

    compatibilité install framework 4.9

--- installeur/generateSetup.bat
+++ installeur/generateSetup.bat
@@ -1,3 +1,14 @@
-C:\Qt\QtIFW-4.6.1\bin\binarycreator --offline-only -c config/config.xml -p packages TiCalculSetup
+@ECHO -------------------------
+@ECHO SETUP generation
+@ECHO -------------------------
+
+REM Qt Installer Framework v4.6
+REM C:\Qt\QtIFW-4.6.1\bin\binarycreator --offline-only -c config/config.xml -p packages TiCalculSetup
+
+REM Qt Installer Framework v4.9
+C:\Qt\Tools\QtInstallerFramework\4.9\bin\binarycreator --offline-only -c config/config.xml -p packages TiCalculSetup
 
 pause
+
+REM Operation Testeur:
+REM C:\Qt\Tools\QtInstallerFramework\4.9\bin\devtool operation <binary> DO,Copy,<source>,<target>
