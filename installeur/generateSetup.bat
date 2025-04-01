fdc920e6337d8a600b09997bd9b987ed2d797ed3
David de Lorenzo <de.lorenzo.david@gmail.com>
2025-04-01 22:30:26 +0200

    Installeur TiCalcul + VCredist - Qt IFW 4.9

--- installeur/generateSetup.bat
+++ installeur/generateSetup.bat
@@ -2,10 +2,6 @@
 @ECHO SETUP generation
 @ECHO -------------------------
 
-REM Qt Installer Framework v4.6
-REM C:\Qt\QtIFW-4.6.1\bin\binarycreator --offline-only -c config/config.xml -p packages TiCalculSetup
-
-REM Qt Installer Framework v4.9
 C:\Qt\Tools\QtInstallerFramework\4.9\bin\binarycreator --offline-only -c config/config.xml -p packages TiCalculSetup
 
 pause
