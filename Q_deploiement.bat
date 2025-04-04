4f494c9b5e1e03e3d2b6428fb6819906446dc432
David de Lorenzo <de.lorenzo.david@gmail.com>
2025-04-04 08:29:50 +0200

    Renommage application en tiCalcul (au lieu de AppTiCalcul)

--- Q_deploiement.bat
+++ Q_deploiement.bat
@@ -2,3 +2,19 @@
 
 windeployqt --release  .\installeur\packages\sphinkie.ticalcul\data
 
+REM OPTIONS:
+REM --pdb :
+REM  --dir <directory>                   Use directory instead of binary directory 
+REM  --libdir <path>                     Copy libraries to path.
+REM  --plugindir <path>                  Copy plugins to path.
+REM  --release                           Assume release binaries.
+REM  --pdb                               Deploy .pdb files (MSVC).
+REM  --compiler-runtime                  Deploy compiler runtime (Desktop only).  
+REM
+REM ARGUMENT:
+REM  [files]                             Binaries or directory containing the binary
+REM 
+REM EXEMPLE:
+REM   windeployqt --pdb tiCalcul.exe --verbose 1 --dir . --plugindir plugins --compiler-runtime
+
+
